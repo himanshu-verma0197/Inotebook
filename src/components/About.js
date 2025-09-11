77103df385de5b9fe9a0fbe7dbd6047a6ebad6e6
himanshu-verma0197 <himu0197@gmail.com>
2025-09-11 21:50:42 +0530

    about us is updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const About = () => { 
+const About = () => {
     return (
         <div>
-            This is About page
+            This is About page. you have to write about the project and about you here.
         </div>
     )
 }
